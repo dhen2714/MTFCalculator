6374f943372f3f71d08a835cf3ecd07fb301e778
dhen2714 <David.Henry3@health.nsw.gov.au>
2023-02-23 16:44:08 +1100

    Active cell writing added.

--- gui/excel.py
+++ gui/excel.py
@@ -3,11 +3,13 @@ from pathlib import Path
 import re
 import xlwings as xw
 import numpy as np
+from pywintypes import com_error
 from .errors import (
     ExcelNotFoundError,
     ValueOverwriteError,
     TemplateWriteError,
     ExcelWriteError,
+    ActiveCellError,
 )
 from .calculator import ColumnIndex
 from .constants import DEFAULT_TEMPLATE_PATH
@@ -45,6 +47,9 @@ class ExcelHandler(ABC):
 class XwingsHandler(ExcelHandler):
     def __init__(self, write_mode="template", params_path=None) -> None:
         self.write_mode = write_mode
+        self.active_sheet = None
+        self.active_cell = None
+        self.active_cell_gen = None
         try:
             self.selected_book = xw.books.active.name
         except xw.XlwingsError:
@@ -66,12 +71,57 @@ class XwingsHandler(ExcelHandler):
             return book_names
         except xw.XlwingsError:
             raise ExcelNotFoundError
+        except com_error:
+            raise ExcelNotFoundError
+
+    def set_active_cell(self) -> None:
+        xwbook = xw.books[self.selected_book]
+        sheet_selection = xwbook.app.selection.sheet
+        if sheet_selection.book.name != self.selected_book:
+            self.active_sheet = None
+            self.active_cell = None
+            self.active_cell_gen = None
+            raise ActiveCellError("Active cell not in selected workbook.")
+
+        self.active_sheet = sheet_selection.name
+        selected_size = xwbook.app.selection.size
+        if selected_size > 1:
+            raise ExcelWriteError
+        self.active_cell = xwbook.app.selection.address.replace("$", "")
+        self.active_cell_gen = self.write_cell_generator(self.active_cell)
+
+    def write_cell_generator(self, active_cell):
+        row_write, col_write = excelkey2ind(active_cell)
+        increment = 5
+        while True:
+            yield (row_write, col_write)
+            col_write += increment
+
+    def write_active(self, file_name: str, mode: str, mtf_data: np.ndarray) -> None:
+        header_rows = np.array(
+            [
+                [file_name, mode, "", "", ""],
+                ["f", "left", "right", "top", "bottom"],
+            ]
+        )
+        write_sheet = xw.books[self.selected_book].sheets[self.active_sheet]
+        write_data = np.concatenate((header_rows, mtf_data))
+        write_cell = next(self.active_cell_gen)
+        try:
+            write_values(write_sheet, write_data, write_cell)
+        except ValueOverwriteError:
+            raise ActiveCellError(
+                "Values detected in cells, cannot write to active cell."
+            )
 
     def write_template(self, mode: str, mtf_data: np.ndarray):
         """Substitution not implemented."""
         book_name = self.selected_book
         sheet_name = self.params_dict["sheet_name"]
-        xwsheet = xw.books[book_name].sheets[sheet_name]
+        try:
+            xwsheet = xw.books[book_name].sheets[sheet_name]
+        except com_error:
+            raise TemplateWriteError
         cell_key = self.params_dict["modes"][mode]
         edge_locations_write = self.params_dict["edge_locations"].split(", ")
         edge_indices = [
@@ -85,48 +135,49 @@ class XwingsHandler(ExcelHandler):
         formatted_data = np.concatenate(array_list, axis=1)
         write_values(xwsheet, formatted_data, cell_key)
 
-        # if contact:
-        #     cell_key = self.params_dict["contact"]
-        #     write_values(xwsheet, contact, cell_key)
-        # if magnification:
-        #     cell_key = self.params_dict["mag"]
-        #     write_values(xwsheet, magnification, cell_key)
-        # if tomo_proj_top:
-        #     cell_key = self.params_dict["tomo_proj_top"]
-        #     write_values(xwsheet, tomo_proj_top, cell_key)
-        # if tomo_proj_bot:
-        #     cell_key = self.params_dict["tomo_proj_bot"]
-        #     write_values(xwsheet, tomo_proj_bot, cell_key)
-        # if tomo_recon_top:
-        #     cell_key = self.params_dict["tomo_recon_top"]
-        #     write_values(xwsheet, tomo_recon_top, cell_key)
-        # if tomo_recon_bot:
-        #     cell_key = self.params_dict["tomo_recon_bot"]
-        #     write_values(xwsheet, tomo_recon_bot, cell_key)
-
     def write_data(self, file_name: str, mode: str, mtf_data: np.ndarray) -> None:
         try:
             if self.write_mode == "template":
                 self.write_template(mode, mtf_data)
+            else:
+                self.write_active(file_name, mode, mtf_data)
         except xw.XlwingsError as e:
             print(e)
             raise ExcelWriteError
-        except TemplateWriteError:
+        except TemplateWriteError as e:
             print(e)
             raise ExcelWriteError
+        except ValueOverwriteError as e:
+            print(e)
+            raise ExcelWriteError
+        except ActiveCellError as e:
+            print(e)
+            self.set_active_cell()
 
 
-def write_values(sheet: xw.Sheet, array: np.ndarray, cell_key: str) -> None:
+def write_values(
+    sheet: xw.Sheet, array: np.ndarray, cell_key: str | tuple[int, int]
+) -> None:
     """Write 2D array to Excel sheet."""
     array_shape = array.shape
-    top_left_index = excelkey2ind(cell_key)
+
+    if type(cell_key) is str:
+        top_left_index = excelkey2ind(cell_key)
+    else:
+        top_left_index = cell_key
+
     endrow, endcol = (
         (top_left_index[0] + array_shape[0] - 1),
         (top_left_index[1] + array_shape[1] - 1),
     )
 
     current_values = np.array(sheet.range(top_left_index, (endrow, endcol)).value)
-    if current_values.any():
-        raise ValueOverwriteError
+    if current_values.astype("object").any():
+        error_string = (
+            f"Values detected in region with {cell_key} "
+            + "as the top-left index. The bottom right (row, column) "
+            + f"index is {endrow, endcol}."
+        )
+        raise ValueOverwriteError(error_string)
     else:
         sheet.range(top_left_index, (endrow, endcol)).value = array
