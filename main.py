ae31a78ee283f1aaa8f01b8c61e0e6ea1e88eb61
achi5416 <David.Henry3@health.nsw.gov.au>
2023-11-01 11:31:00 +1100

    Moved files.

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from gui.model import Model
 from gui.presenter import Presenter
 from gui.view import MTFCalculator
 from gui.excel import XwingsHandler
+from pathlib import Path
 
+TEMPLATE_PATH = Path(__file__).parent / "template_parameters.json"
 
 def main() -> None:
-    excel_handler = XwingsHandler()
-    calculator = MammoTemplateCalc()
+    excel_handler = XwingsHandler(TEMPLATE_PATH)
+    calculator = MammoTemplateCalc(TEMPLATE_PATH)
     model = Model(mtf_calculator=calculator, excel_handler=excel_handler)
     view = MTFCalculator()
     presenter = Presenter(model, view)
--- src/gui/utils.py
+++
@@ -1,7 +0,0 @@
-import json
-from pathlib import Path
-
-
-def read_json(fpath: str | Path) -> dict:
-    with open(fpath, "r") as f:
-        return json.load(f)
