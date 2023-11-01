8cca9b359fa26e8497e12281da947d68d7e828ee
achi5416 <David.Henry3@health.nsw.gov.au>
2023-11-01 11:39:31 +1100

    Formatting.

--- gui/view.py
+++ gui/view.py
@@ -47,13 +47,16 @@ class Presenter(Protocol):
     def handle_active_cell_refresh(self) -> None:
         ...
 
+
 class cTkdnd(ctk.CTk, TkinterDnD.DnDWrapper):
     def __init__(self, fg_color: str | Tuple[str, str] | None = None, **kwargs):
         super().__init__(fg_color, **kwargs)
         self.TkdndVersion = TkinterDnD._require(self)
 
+
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
+
 
 class MTFCalculator(cTkdnd):
     def __init__(self) -> None:
@@ -86,7 +89,9 @@ class MTFCalculator(cTkdnd):
         self.button_clear_all.pack(side=tk.LEFT)
         self.image_list_buttons.pack()
 
-        self.workbook_frame = ctk.CTkFrame(self.frame, width=300, height=450, fg_color="transparent")
+        self.workbook_frame = ctk.CTkFrame(
+            self.frame, width=300, height=450, fg_color="transparent"
+        )
         self.workbook_option_label = ctk.CTkLabel(
             self.workbook_frame, text="Selected Excel workbook:"
         )
@@ -106,7 +111,9 @@ class MTFCalculator(cTkdnd):
         # Frame that contains the 'template' and 'active' radiobuttons for Excel write.
         self.write_mode_frame = ctk.CTkFrame(self, fg_color="transparent")
         self.write_mode = ctk.StringVar(self, value="template")
-        self.write_mode_label = ctk.CTkLabel(self.write_mode_frame, text="Write data to:")
+        self.write_mode_label = ctk.CTkLabel(
+            self.write_mode_frame, text="Write data to:"
+        )
         self.template_write = ctk.CTkRadioButton(
             self.write_mode_frame,
             text="Template",
