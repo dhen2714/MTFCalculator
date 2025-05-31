3bb5d5f22db7f5b23320e739118719dccf05000d
dhen2714 <davhe35@gmail.com>
2025-05-31 17:30:57 +1000

    Pyinstaller decompress.

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from gui.presenter import Presenter
 from gui.view import MTFCalculator
 from gui.excel import XwingsHandler
 from pathlib import Path
+import os
+import sys
+
+if getattr(sys, "frozen", False):
+    # If running in PyInstaller bundle
+    print(sys._MEIPASS)
+    dll_path = os.path.join(sys._MEIPASS, "bin")
+    os.add_dll_directory(dll_path)
 
 TEMPLATE_PATH = Path(__file__).parent / "template_parameters.json"
 
